41b52a5c3916ec4f4300008cc2c58e64f9a233a7
pavel <gritsatsuev.pavel@gmail.com>
2023-05-11 03:32:38 +0400

    add php-cs-fixer and create script for init config

---
+++ scripts/php-cs-fixer-configuration.php
@@ -0,0 +1,12 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = new Symfony\Component\Dotenv\Dotenv();
+$dotenv->load(__DIR__ . '/../.env');
+
+if ($_ENV['APP_ENV'] === 'dev') {
+    if (!file_exists('.php-cs-fixer.php')) {
+        copy('.php-cs-fixer.dist.php', '.php-cs-fixer.php');
+    }
+}
